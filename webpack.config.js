9208f295ff624598f93b3df29c40d5e4033ee72d
Troy Harvey <troyharvey@gmail.com>
2019-12-19 23:50:39 -0500

    Add plyr css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
     entry: './src/widget.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
